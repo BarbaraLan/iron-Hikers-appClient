8fa8d82c27791b1d047a0281bd14ee46b3867dde
Barbara Lan <barbie.lancuba@gmail.com>
2024-03-07 12:36:07 +0100

    jiji

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './style/App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
-import DashboardPage  from './pages/DashboardPage'
+import DashboardPage from './pages/DashboardPage'
 import Footer from './components/Footer'
 import AboutPage from './pages/AboutPage'
 
@@ -20,13 +20,13 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/dashboard' element={<DashboardPage />} />
         <Route path='/About' element={<AboutPage />} />
-        <Route path='/login' element={<LogInPage/>} />
-        <Route path='/signup' element={<SignUpPage/>} />
-     <Route path='/create-hike' element={<CreateHikePage />} />
-   
+        <Route path='/login' element={<LogInPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/create-hike' element={<CreateHikePage />} />
+
       </Routes>
-      
-        <Footer/>
+
+      <Footer />
 
     </>
   )
---
+++ src/Pages/RoutesPage.jsx
@@ -0,0 +1,11 @@
+import '../style/Routes.css'
+
+function Routes() {
+    return (
+        <>
+          <h3> this is the Routes Page</h3>
+
+        </>
+    )
+}
+export default Routes;
