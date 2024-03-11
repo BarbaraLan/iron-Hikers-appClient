4658e408367e3fa196acb3b99df9b0e5c4820160
Barbara Lan <barbie.lancuba@gmail.com>
2024-03-11 17:18:21 +0100

    city done

--- src/Pages/City.jsx
+++ src/Pages/City.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '../style/City.css';
 import axios from 'axios';
+import { AuthContext } from '../context/auth.context';
 
 // npm install countries-list gives you all countries says AI. will put on backlog to try
 
@@ -8,92 +9,86 @@ import axios from 'axios';
 function CityPage() {
 
     const [selectedCountry, setSelectedCountry] = useState('');
-    const [selectedRegion, setSelectedRegion] = useState('');
     const [selectedCity, setSelectedCity] = useState('');
+    const [countryCities, setCountryCities] = useState([])
 
-    const countryOptions = ['Country 1', 'Country 2', 'Country 3']; // Replace with your country options
-    const cityOptions = {
-        'Country 1': {
-            'Region 1': ['City 1-1-1', 'City 1-1-2', 'City 1-1-3'],
-            'Region 2': ['City 1-2-1', 'City 1-2-2', 'City 1-2-3']
-        },
-        'Country 2': {
-            'Region 1': ['City 2-1-1', 'City 2-1-2', 'City 2-1-3'],
-            'Region 2': ['City 2-2-1', 'City 2-2-2', 'City 2-2-3']
-        },
-        'Country 3': {
-            'Region 1': ['City 3-1-1', 'City 3-1-2', 'City 3-1-3'],
-            'Region 2': ['City 3-2-1', 'City 3-2-2', 'City 3-2-3']
-        }
-    };
+    const { userInfo } = useContext(AuthContext)
+    const userId = userInfo._id
 
+    const countryCitiesMap = {
+        'Spain': ['Barcelona', 'Madrid'],
+        'France': ['Paris'],
+        'Scotland': ['Edimburgh'],
+        'Italy': ['Rome']
+    }
 
     const handleCountryChange = (event) => {
-        setSelectedCountry(event.target.value);
-        setSelectedRegion('');
-        setSelectedCity('');
-    };
+        const country = event.target.value
+        setSelectedCountry(country);
+        setCountryCities(countryCitiesMap[country]);
 
-    const handleRegionChange = (event) => {
-        setSelectedRegion(event.target.value);
-        setSelectedCity('');
-    };
+    }
 
-    const handleCityChange = async (event) => {
+    const handleCityChange = (event) => {
         const city = event.target.value;
         setSelectedCity(city);
 
-    try {
-       
-        const response = await axios.post('http://localhost:5000/api/user/update', {
-            userID: 'user_id', 
-            city: city
-        });
-        console.log('City updated successfully. Response:', response.data);
-    } catch (error) {
-        console.error('Error updating city:', error);
+        
     }
-};
-
     
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        
+        const userData = {
+            userId: userInfo._id,
+            city: selectedCity
+        };
+        
+        axios
+        .put(('http://localhost:5005/api/user/update'), userData)
+        .then((response) => {
+            setSelectedCity(response.data)
+        })
+        .catch((error) => error)
+    }
 
     return (
-        <div>
+        <>
             <h2>Select Your City! </h2>
 
-            <label value="country">Country:</label>
-            <select id="country" value={selectedCountry} onChange={handleCountryChange}>
-                <option value="">Select a Country</option>
-                {countryOptions.map((country, index) => (
-                    <option key={index} value={country}>
-                        {country}
-                    </option>
-                ))}
-            </select>
+            <div>
+                <label>Country:</label>
+                <select value={selectedCountry} onChange={handleCountryChange}>
+                    <option value=""> Select a Country</option>
 
-            <label value="region">Region:</label>
-            <select id="region" value={selectedRegion} onChange={handleRegionChange} disabled={!selectedCountry}>
-                <option value="">Select a Region</option>
-                {selectedCountry && Object.keys(cityOptions[selectedCountry]).map((region, index) => (
-                    <option key={index} value={region}>
-                        {region}
-                    </option>
-                ))}
-            </select>
 
-            <label value="city">City:</label>
-            <select id="city" value={selectedCity} onChange={handleCityChange} disabled={!selectedRegion}>
-                <option value="">Select a City</option>
-                {selectedRegion && cityOptions[selectedCountry][selectedRegion].map((city, index) => (
-                    <option key={index} value={city}>
-                        {city}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+                    {Object.keys(countryCitiesMap).map((country) => (
+                        <option key={country} value={country}>
+                            {country}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div>
+                <label>City:</label>
+                <select value={selectedCity} onChange={handleCityChange}>
+                    <option value=""> Select a City</option>
+
+                    {countryCities.map((city) => (
+                        <option key={city} value={city}>
+                            {city}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <button onClick={handleSubmit}> submit city </button>
+        </>
+    )
+
+
 }
-
 export default CityPage;
 
 
