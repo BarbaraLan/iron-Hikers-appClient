e70842bc25a22fca46586f446e738e5eb9a321d2
Kumar Daryanani <destral@msn.com>
2024-03-14 11:46:58 +0100

    Populate attendee names in HikeListCard, Edit/Delete Hike buttons, Delete Hike functionality first iteration

---
+++ src/components/UserJoinedHikes.jsx
@@ -0,0 +1,19 @@
+import HikeListCard from "./HikeListCard";
+import { useEffect, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+
+
+function UserJoinedHikes() {
+
+    const { userInfo } = useContext(AuthContext)
+    const userId = userInfo._id;
+
+    useEffect(()=>{
+
+    }, [])
+    return(
+        {/*Map the user's joinedHikes array and display a HikeListCard for each with the appropriate info*/}
+    )
+}
+
+export default UserJoinedHikes;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -7,6 +7,7 @@ function DashboardPage () {
             <h2> Choose your path </h2>
         
         <div className='dash-box'>
+            {/*TO-DO: Add hikes the user is going to, in chronological order. Can reuse the code for Upcoming Hikes, but filter them by whether the user is in the attendees array*/}
         <Link className='JoinHike' to='/join-hike'> Join Hike </Link>
         <Link className='CreateHike' to='/hikes/create'> Create Hike </Link>
         </div>
